70f08e36e9bde10a322ab4dd3be7135cada2b11d
C-3PFLO <c.3pflo.1@gmail.com>
2024-04-15 23:34:14 -0400

    empty story component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+// infrastructure
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// view components
+import { Story } from './view/Story';
+
+// setup root element
 const rootElement = document.createElement('div');
 document.body.appendChild(rootElement);
-
 const reactRoot = ReactDOM.createRoot(rootElement);
+
+// render
 reactRoot.render(
     <React.StrictMode>
-        Hello world!
+        <Story></Story>
     </React.StrictMode>
 );
